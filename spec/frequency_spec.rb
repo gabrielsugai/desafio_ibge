7d42cb0eb84620c86e5a2615f8e67e40bdd5f1a9
Gabriel Sugai <gabrielsugai@gmail.com>
2020-07-29 20:08:48 -0300

    Refatora testes de output.

--- spec/frequency_spec.rb
+++ spec/frequency_spec.rb
@@ -3,17 +3,17 @@ require 'spec_helper'
 describe Frequency do
 
     it 'Should check the frequency of a name over the years' do
-        expect { Frequency.of_name('gabriel') }.to output(
-"|  Periodo     |   gabriel |
-    1930[      |   1267
- [1930,1940[   |   2744
- [1940,1950[   |   4256
- [1950,1960[   |   6095
- [1960,1970[   |   6559
- [1970,1980[   |   9103
- [1980,1990[   |   46996
- [1990,2000[   |   271405
- [2000,2010[   |   584024\n").to_stdout
+        expect { Frequency.of_name('gabriel') }.to output(<<-MESSAGE.strip_heredoc).to_stdout
+           |  Periodo     |   gabriel |
+               1930[      |   1267
+            [1930,1940[   |   2744
+            [1940,1950[   |   4256
+            [1950,1960[   |   6095
+            [1960,1970[   |   6559
+            [1970,1980[   |   9103
+            [1980,1990[   |   46996
+            [1990,2000[   |   271405
+            [2000,2010[   |   584024
+        MESSAGE
     end
-
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -4,11 +4,12 @@ require './lib/partials/menu'
 describe Partials::Menu do
 
     it 'Should show a menu for users' do
-        expect { Partials::Menu.new.show_menu }.to output(
-"Digite 1 para verificar o ranking de nomes de uma UF.
-Digite 2 para verificar o ranking de nomes de um MU.
-Digite 3 para verificar a frequencia de um nome ao decorer dos anos.
-Digite 4 para sair.\n").to_stdout
+        expect { Partials::Menu.new.show_menu }.to output(<<-MESSAGE.strip_heredoc).to_stdout
+            Digite 1 para verificar o ranking de nomes de uma UF.
+            Digite 2 para verificar o ranking de nomes de um MU.
+            Digite 3 para verificar a frequencia de um nome ao decorer dos anos.
+            Digite 4 para sair.
+        MESSAGE
     end
 
 end
