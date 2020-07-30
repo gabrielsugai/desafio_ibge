631e3fc2afbb5cb45d811c1993bd41cbfcd5c915
Gabriel Sugai <gabrielsugai@gmail.com>
2020-07-29 23:37:16 -0300

    Adiciona busca de frequencia de nomes para mais de um nome por vez.

--- lib/frequency.rb
+++ lib/frequency.rb
@@ -8,18 +8,53 @@ class Frequency
         @name = name
     end
 
-    def self.of_name(name)
-        result = []
-        response = Faraday.get("https://servicodados.ibge.gov.br/api/v2/censos/nomes/#{name}")
-        json = JSON.parse(response.body, symbolize_names: true)
-        json = json.first[:res]
-        puts("|  Periodo     |   #{name} |")
-        json.each do |data|
-            if data[:periodo].length == 5
-                puts("    #{data[:periodo]}      |   #{data[:frequencia]}")
-            else
-                puts(" #{data[:periodo]}   |   #{data[:frequencia]}")
+    def self.of_name(names)
+        aux = ''
+        if names.length == 1
+            aux = names.first
+        elsif names.length >= 2
+            names.each do |name|
+                aux += "#{name}%7C"
             end
+        end
+        name_list = []
+        period = []
+        frequency = []
+        response = Faraday.get("https://servicodados.ibge.gov.br/api/v2/censos/nomes/#{aux}")
+        json = JSON.parse(response.body, symbolize_names: true)
+        json.each do |name_frequency|
+            name_list << name_frequency[:res]
+        end
+        name_list.first.each do |name|
+            period << name[:periodo]
+        end
+        name_list.each do |name|
+            aux_list = []
+            name.each do |x|
+                aux_list << x[:frequencia]
+            end
+            frequency << aux_list
+        end
+        table_head = "| Periodo   "
+        names.each do |name|
+            aux = "  #{name} "
+            table_head += aux
+        end
+        table_head += "|"
+        puts(table_head)
+
+        period.each do |decade|
+            if decade.length == 5
+                message = "    #{decade}    "
+            else
+                message = " #{decade} "
+            end
+            frequency.each do |amount|
+                aux = "  #{amount.first}"
+                message += aux
+                amount.shift
+            end
+            puts(message)
         end
     end
 end
--- spec/frequency_spec.rb
+++ spec/frequency_spec.rb
@@ -3,17 +3,36 @@ require 'spec_helper'
 describe Frequency do
 
     it 'Should check the frequency of a name over the years' do
-        expect { Frequency.of_name('gabriel') }.to output(<<-MESSAGE.strip_heredoc).to_stdout
-           |  Periodo     |   gabriel |
-               1930[      |   1267
-            [1930,1940[   |   2744
-            [1940,1950[   |   4256
-            [1950,1960[   |   6095
-            [1960,1970[   |   6559
-            [1970,1980[   |   9103
-            [1980,1990[   |   46996
-            [1990,2000[   |   271405
-            [2000,2010[   |   584024
+        names = ['gabriel']
+        expect { Frequency.of_name(names) }.to output(<<-MESSAGE.strip_heredoc).to_stdout
+           | Periodo     gabriel |
+               1930[      1267
+            [1930,1940[   2744
+            [1940,1950[   4256
+            [1950,1960[   6095
+            [1960,1970[   6559
+            [1970,1980[   9103
+            [1980,1990[   46996
+            [1990,2000[   271405
+            [2000,2010[   584024
+        MESSAGE
+    end
+
+    it 'Should check the frequency of more than one name over the years' do
+        names = ['gabriel', 'joao']
+        expect { Frequency.of_name(names) }.to output(<<-MESSAGE.strip_heredoc).to_stdout
+            | Periodo     gabriel   joao |
+                1930[      1267  60155
+             [1930,1940[   2744  141772
+             [1940,1950[   4256  256001
+             [1950,1960[   6095  396438
+             [1960,1970[   6559  429148
+             [1970,1980[   9103  279975
+             [1980,1990[   46996  273960
+             [1990,2000[   271405  352552
+             [2000,2010[   584024  794118
         MESSAGE
     end
 end
+
+
